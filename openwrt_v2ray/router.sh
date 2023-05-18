64c9d03888b50acbf217a53e3749a9ced730d2d2
杜健 <dujian.0327@bytedance.com>
2023-05-18 16:41:07 +0800

    nas config
    Change-Id: I5f39384285c34e5ba1f148783dbf8d45cb6c60a3

--- openwrt_v2ray/router.sh
+++
@@ -1,4 +0,0 @@
-echo 0 > /proc/sys/net/bridge/bridge-nf-call-iptables
-echo 0 > /proc/sys/net/bridge/bridge-nf-call-ip6tables
-echo 0 > /proc/sys/net/bridge/bridge-nf-call-arptables
-echo 0 > /proc/sys/net/bridge/bridge-nf-call-custom

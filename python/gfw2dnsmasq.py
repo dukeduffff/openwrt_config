5c0001f0838f1aeaa97492a29cfcd968036cc928
杜健 <dujian.0327@bytedance.com>
2023-12-18 11:08:14 +0800

    feat: 新的配置
    Change-Id: I5d11a432b8d11e6b430809dc4d1f3865002b31f2

--- python/gfw2dnsmasq.py
+++ python/gfw2dnsmasq.py
@@ -6,9 +6,9 @@
 # Copyright (C) 2014 http://www.shuyz.com
 # Ref https://code.google.com/p/autoproxy-gfwlist/wiki/Rules
 # https://gist.github.com/lanceliao/85cd3fcf1303dba2498c
-import datetime
-import sys
+import argparse
 import typing
+from datetime import datetime
 
 import requests
 
@@ -80,7 +80,7 @@ def common_gfw_txt_to_conf():
 
     fs = open("./gfwlist.conf", "w")
     fs.write('# gfw list ipset rules for dnsmasq\n')
-    fs.write('# updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
+    fs.write('# updated on ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
     fs.write('#\n')
 
     for domain in content.split("\n"):
@@ -88,7 +88,7 @@ def common_gfw_txt_to_conf():
         if not domain:
             continue
         fs.write(f"server=/{domain}/{mydnsip}#{mydnsport}\n")
-        fs.write(f"ipset=/{domain}/{ipset_name},{ipset6_name}\n")
+        # fs.write(f"ipset=/{domain}/{ipset_name},{ipset6_name}\n")
     fs.close()
     print("generate gfwlist common config success!")
 
@@ -142,18 +142,12 @@ def ping_shell(host, cnt):
     return avg_time, loss_rate
 
 
-def get_cfnode_hosts(get_remote=True):
-    # 生成默认比较稳定的机器
-    hosts: typing.List[HostScore] = [
-        HostScore(host="172.67.171.3", speed=100),
-        # HostScore(host="104.16.14.169", speed=100),
-        # HostScore(host="172.64.80.1", speed=100),
-        # HostScore(host="172.67.5.233", speed=60)  # 香港节点
-    ]
+def get_ipv4_cfnode_hosts(get_remote=True):
+    hosts = []
     if not get_remote:
         return hosts
     try:
-        res = requests.get("https://monitor.gacjie.cn/api/client/get_ip_address", timeout=10)
+        res = requests.get("https://monitor.gacjie.cn/api/ajax/get_cloud_flare_v4?page=1&limit=10", timeout=10)
     except Exception:
         return hosts
     if res.status_code != 200:
@@ -161,12 +155,14 @@ def get_cfnode_hosts(get_remote=True):
     res_json = res.json()
     if not res_json.get("status", False):
         return hosts
-    res_list = res_json.get("info", {}).get("CM")
-    for h in res_list:
-        address = h.get("ip", "")
-        if not address:
+    ips = res_json.get("data", [])
+    for ip in ips:
+        if ip.get("device_name") != "山东移动":
             continue
-        hosts.append(HostScore(host=address, speed=100))
+        hosts.append(HostScore(
+            host=ip.get("address"),
+            speed=ip.get("speed", 0) / 100
+        ))
     return hosts
 
 
@@ -193,7 +189,7 @@ def get_ipv6_cfnode_hosts():
 
 
 def chose_best_host(domains=None, ipv6=False) -> typing.Tuple[str, bool]:
-    hour = datetime.datetime.now().hour
+    hour = datetime.now().hour
     # 每天18点到次日凌晨1点,使用该ip,目前测试下来最快的
     # if hour >= 18 or hour <= 1:
     #     return "172.67.171.3"
@@ -205,7 +201,7 @@ def chose_best_host(domains=None, ipv6=False) -> typing.Tuple[str, bool]:
         is_ipv6 = True if hosts else False
     elif not hosts:
         if not domains:
-            hosts.extend(get_cfnode_hosts())
+            hosts.extend(get_ipv4_cfnode_hosts())
         else:
             for domain in domains:
                 hosts.extend(get_domain_ip_v4(domain))
@@ -244,43 +240,68 @@ def dns_resolver(domain, ip_type=A):
     return hosts
 
 
-def custom_gfw_to_conf():
+def replace_template(from_dir, to_dir, chose_ipv6, url):
+    minute = datetime.now().minute
+    if minute % 10 != 0:
+        if url and check_gfw(url=url):
+            exit(0)
+
+    best_host, is_ipv6 = chose_best_host(ipv6=chose_ipv6)
+    lines = open(from_dir, "r")
+    to_file = open(to_dir, "w")
+    for line in lines:
+        to_file.write(line.replace("{CF_IP}", best_host))
+    to_file.close()
+    exit(1)
+
+
+def check_gfw(url):
+    try:
+        requests.get(url, timeout=5)
+    except Exception:
+        return False
+    return True
+
+
+def custom_gfw_to_conf(chose_ipv6):
     fs = open("./gfw_custom.conf", "w")
     # 写入静态文件
     fs.write("""
 # gfw server\n""")
     # 写入出国最优ip
-    chose_ipv6 = True
     best_host, is_ipv6 = chose_best_host(ipv6=chose_ipv6)
     fs.write(f"address=/cntest2022.cf/{'0.0.0.0' if is_ipv6 else best_host}\n")
     fs.write(f"address=/cntest2022.cf/{best_host if is_ipv6 else '::'}\n")  # 禁止ipv6解析
     fs.write("""
 # gfw dns
-server=/monitor.gacjie.cn/127.0.0.1#5153
 
 # gfw custom
 server=/supes.top/127.0.0.1#5153
-ipset=/supes.top/gfwlist,gfwlist6
 server=/openwrt.ai/127.0.0.1#5153
-ipset=/openwrt.ai/gfwlist,gfwlist6
 server=/369369.xyz/127.0.0.1#5153
-ipset=/369369.xyz/gfwlist,gfwlist6
 server=/adguard.com/127.0.0.1#5153
-ipset=/adguard.com/gfwlist,gfwlist6
-server=/69shu.com/127.0.0.1#5153
-ipset=/69shu.com/gfwlist,gfwlist6\n""")
+server=/69shu.com/127.0.0.1#5153\n""")
     fs.close()
     print("generate gfwlist custom config success!")
 
 
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) > 1:
-        arg = args[1]
-        if arg == "common":
-            common_gfw_txt_to_conf()
-        elif arg == "custom":
-            custom_gfw_to_conf()
-    else:
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("type", help="执行类型", choices=["ipset_gfw", "ipset_custom", "xray", "check"])
+    parser.add_argument("-tpl", "--template", help="模版路径, only for xray")
+    parser.add_argument("-td", "--target", help="配置写入路径, only for xray")
+    parser.add_argument("-6", "--ipv6", action="store_true", help="生成ipv6, 否则生成ipv4")
+    parser.add_argument("-url", "--url", help="测试url, only for check")
+
+    args = parser.parse_args()
+
+    if args.type == "ipset_gfw":
         common_gfw_txt_to_conf()
-        custom_gfw_to_conf()
+    elif args.type == "ipset_custom":
+        custom_gfw_to_conf(args.ipv6)
+    elif args.type == "xray":
+        replace_template(args.template, args.target, args.ipv6, args.url)
+
+
+if __name__ == '__main__':
+    parse_args()
---
+++ python/xray.md
@@ -0,0 +1,34 @@
+
+## 定时更新gfw脚本
+```shell
+10 * * * * bash /root/passwall/update.sh
+
+#!/bin/bash
+cd /root/passwall
+python gfw2dnsmasq.py ipset_gfw
+cp /root/passwall/gfwlist.conf /tmp/dnsmasq.d/
+/etc/init.d/dnsmasq restart
+```
+
+## 定时更新自定义ipset
+```shell
+*/10 * * * * bash /root/passwall/update_custom.sh
+
+#!/bin/bash
+cd /root/passwall
+python gfw2dnsmasq.py ipset_custom
+cp /root/passwall/gfw_custom.conf /tmp/dnsmasq.d/
+/etc/init.d/dnsmasq restart
+/etc/init.d/xray restart
+```
+
+## 定时更新xray配置
+```shell
+*/10 * * * * bash /root/passwall/update_xray.sh
+
+cd /root/passwall
+python gfw2dnsmasq.py xray -tpl /root/passwall/template.json -td /root/passwall/xray/config.json -url https://www.google.com
+if [ $? -eq 1 ]; then
+    /etc/init.d/xray restart
+fi
+```
